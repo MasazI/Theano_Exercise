1bd1e67e60d15283adcbae0eaa791903640f76ca
masazdream <masai@masai-no-MacBook-Pro.local>
2015-11-01 23:07:07 +0900

    add scan example.

--- theano_scan.py
+++ theano_scan.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 import theano
 import theano.tensor as T
-
+from theano import config
 import numpy as np
 
 ## scan.Loop for int
@@ -39,17 +39,71 @@ outputs = T.as_tensor_variable(np.asarray(0))
 # sequencesはシーケンスとしてIterationされる引数であり、ここでは配列のシンボルを指定。配列の各要素に処理がIterationされる
 # outputs_infoはLoop処理の初期値
 c = T.scalar('c', dtype='int64')
-result, updates = theano.scan(fn=lambda seq, prior: seq + 2, sequences = T.arange(c), outputs_info=outputs, non_sequences=None)
+[result, updates] = theano.scan(fn=lambda seq, prior: seq + 2, sequences = T.arange(c), outputs_info=outputs, non_sequences=None)
 sf3 = theano.function(inputs=[c], outputs=result, updates=updates)
 # 0,1,2,3,4の配列の各要素に2ずつ加算して結果をえる
 print sf3(5)
 
 ## scan.Iteration using prior
 # シーケンスの場合の第２引数priorを使うと、前回の結果を引き継いで計算できる
-result, updates = theano.scan(fn=lambda seq, prior: seq + prior, sequences = T.arange(c), outputs_info=outputs, non_sequences=None)
+[result, updates] = theano.scan(fn=lambda seq, prior: seq + prior, sequences = T.arange(c), outputs_info=outputs, non_sequences=None)
 # n_stepsを指定して強制的に処理を終了させることが可能、その場合戻り値の要素はn_stepsで実行した分になる
 # result, updates = theano.scan(fn=lambda seq, prior: seq + prior, sequences = T.arange(c), outputs_info=outputs, non_sequences=None, n_steps=3)
 
 sf4 = theano.function(inputs=[c], outputs=result, updates=updates)
 # 0,1,2,3,4の配列の各要素に、前回までの要素の和を加算して結果をえる
 print sf4(5)
+
+
+## scan.Iteration for vector using prior and return
+outputs = T.as_tensor_variable(np.asarray(0))
+x = T.ivector('x')
+def recurrence(x_t, h_tm1):
+    print x_t.eval
+    print h_tm1.eval
+    h_t = x_t + h_tm1
+    s_t = x_t * 10
+    return [h_t, s_t]
+
+[h, s] = theano.scan(fn=recurrence, sequences=x, outputs_info=[outputs, None])
+sf5 = theano.function(inputs=[x], outputs=h, updates=s)
+print sf5([1,2,3,4,5])
+
+
+outputs = T.as_tensor_variable(np.asarray(0))
+def recurrence_lstm(m_, x_, h_, c_):
+    #print m_.eval
+    #print x_.eval
+    #print h_.eval
+    #print c_.eval
+    h = m_ + h_
+    c = x_ + c_
+    return h, c
+
+m = T.lvector('m')
+x = T.lvector('x')
+a = T.as_tensor_variable(np.asarray(0))
+b = T.as_tensor_variable(np.asarray(0))
+
+rval, updates = theano.scan(fn=recurrence_lstm, sequences=[m, x], outputs_info=[a, b], n_steps=5)
+sf6 = theano.function(inputs=[m, x], outputs=rval, updates=updates)
+print sf6([0,1,2,3,4], [10,11,12,13,14])
+
+
+## scan be used as repeat-until
+def power_of_2(previous_power, max_value):
+    #return previous_power*2, theano.scan_module.until(previous_power*2 > max_value)
+    return previous_power*2
+max_value = T.scalar()
+values,_= theano.scan(fn=power_of_2,
+                        outputs_info = T.constant(1.),
+                        non_sequences = max_value,
+                        n_steps = 10)
+
+f = theano.function([max_value], values)
+
+#print _
+
+print f(45)
+
+
